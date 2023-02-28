cc63b9a274cda8515b82ee81e89ae1edff2a133a
Brandon Coston <costonb@gmail.com>
2023-02-27 21:55:10 -0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Brandon Coston on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
